73e4903af3758254f946ea79461502ba84b723bf
Nathan Thomas <nwthomas@me.com>
2019-03-05 12:41:21 -0800

    Update with new client to fix eslint bug

---
+++ client/src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div>
+        <button type="button">Strike</button>
+        <button type="button">Ball</button>
+        <button type="button">Foul</button>
+        <button type="button">Hit</button>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;

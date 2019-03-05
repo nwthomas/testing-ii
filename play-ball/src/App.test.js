eb85d0357c10370e9d8e1833658f6dcf73dad9e9
Nathan Thomas <nwthomas@me.com>
2019-03-05 12:11:17 -0800

    Run CRA and buid out initial file directory

---
+++ play-ball/src/App.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import renderer from "react-test-renderer";
+
+describe("<App />", () => {
+  it("should match snapshot", () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});

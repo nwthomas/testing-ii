9d2f1cb1be5157235dd170ad955a0cc946926d8e
Nathan Thomas <nwthomas@me.com>
2019-03-05 21:36:18 -0800

    Update testing suite in display.test.js

--- client/src/Components/Display/Display.test.js
+++ client/src/Components/Display/Display.test.js
@@ -1,20 +1,25 @@
 import React from "react";
-import { render } from "react-testing-library";
+import { cleanup, render } from "react-testing-library";
 import "jest-dom/extend-expect";
 
 import Display from "./Display";
-import { Dashboard } from "../Dashboard";
+
+afterEach(cleanup); // Equivalent to componentWillUnmount for this testing library
 
 describe("<Display />", () => {
   it("should display the count of strikes, balls, and outs", () => {
-    const { getByTestId, rerender } = render(
-      <Display stateOnProps={{ strikes: 0, balls: 0, outs: 0 }} />
-    );
+    const { getByTestId } = render(<Display stateOnProps={{ strikes: 0 }} />);
     const strikes = getByTestId("strikes");
-    const balls = getByTestId("balls");
-    const outs = getByTestId("outs");
     expect(strikes.textContent).toBe("0"); // Testing initial 0 status from stateOnProps
+  });
+  it("should display the count of balls", () => {
+    const { getByTestId } = render(<Display stateOnProps={{ balls: 0 }} />);
+    const balls = getByTestId("balls");
     expect(balls.textContent).toBe("0");
+  });
+  it("should display the count of outs", () => {
+    const { getByTestId } = render(<Display stateOnProps={{ outs: 0 }} />);
+    const outs = getByTestId("outs");
     expect(outs.textContent).toBe("0");
   });
 });
